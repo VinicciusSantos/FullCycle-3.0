8e1741c59e939db9a26e6235ee6c1ca3d83c11e1
Vinícius Guedes <comercial.viniciusguedes@gmail.com>
2023-08-07 17:29:08 -0300

    [cap 04] 🔄| criando serviços grpc

--- 04 | Comunicação entre sistemas 🔄/gRPC/internal/database/category.go
+++ 04 | Comunicação entre sistemas 🔄/gRPC/internal/database/category.go
@@ -27,6 +27,15 @@ func (c *Category) Create(name string, description string) (Category, error) {
 	return Category{ID: id, Name: name, Description: description}, nil
 }
 
+func (c *Category) Find(categoryId string) (Category, error) {
+	var id, name, description string
+    err := c.db.QueryRow("SELECT * FROM categories c WHERE c.id = $1", categoryId).
+    Scan(&id, &name, &description)
+    if err != nil {
+        return Category{}, err
+    }
+    return Category{ID: id, Name: name, Description: description}, nil
+}
 
 func (c *Category) FindAll() ([]Category, error) {
 	rows, err := c.db.Query("SELECT * FROM categories")
