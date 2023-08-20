d81dd5c24ef696144321c746bb8d7b9f17c73da8
Vinicius Guedes <comercial.viniciusguedes@gmail.com>
2023-08-20 15:53:07 -0300

    [cap 09] 🏗 | criando serviço de produtos

--- 09 | Arquitetura Hexagonal 🏗/application/product.go
+++ 09 | Arquitetura Hexagonal 🏗/application/product.go
@@ -2,7 +2,14 @@ package application
 
 import (
 	"errors"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
 )
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
 
 type ProductInterface interface {
 	IsValid() (bool, error)
@@ -20,22 +27,62 @@ const (
 )
 
 type Product struct {
-	ID string
-	Name string
-	Status string
-	Price float64
+	ID string `valid:"uuidv4"` 
+	Name string `valid:"required"`
+	Status string `valid:"required"`
+	Price float64 `valid:"float,optional"`
+}
+
+type ProductServiceInterface interface {
+	Get(ID string) (ProductInterface, error)
+	Create(name string, price float64) (ProductInterface, error)
+	Enable(product ProductInterface) (bool, error)
+	Disable(product ProductInterface) (bool, error)
+}
+
+type ProductReader interface {
+	Get(ID string) (ProductInterface, error)
+}
+
+type ProductWriter interface {
+	Save(product ProductInterface) (ProductInterface, error)
+}
+
+type ProductPersistenceInterface interface {
+	ProductReader
+	ProductWriter
+}
+
+func NewProduct() *Product {
+	return &Product{
+		ID: uuid.NewV4().String(),
+		Status: DISABLED,
+	}
 }
 
 func (p *Product) IsValid() (bool, error) {
-	if p.Price > 0 && p.Name != "" {
-		return true, nil
+	if p.Status == "" {
+		p.Status = DISABLED
 	}
 
-	return false, nil
+	if p.Status != ENABLED && p.Status != DISABLED {
+		return false, errors.New("The status must be enabled or disabled")
+	}
+
+	if p.Price < 0 {
+		return false, errors.New("The price must be greater than zero")
+	}
+
+	_, err := govalidator.ValidateStruct(p)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
 }
 
 func (p *Product) Enable() error {
-	if p.Price >= 0 {
+	if p.Price > 0 {
 		p.Status = ENABLED
 		return nil
 	}
@@ -43,8 +90,11 @@ func (p *Product) Enable() error {
 }
 
 func (p *Product) Disable() error {
-	p.Status = DISABLED
-	return nil
+	if p.Price == 0 {
+		p.Status = DISABLED
+		return nil
+	}
+	return errors.New("The price must be zero to disable the product")
 }
 
 func (p *Product) GetID() string {
--- 09 | Arquitetura Hexagonal 🏗/application/product_test.go
+++ 09 | Arquitetura Hexagonal 🏗/application/product_test.go
@@ -1,17 +1,67 @@
 package application_test
 
 import (
-	"github.com/vinicciussantos/arquitetura-hexagonal/application"
+	"fmt"
 	"testing"
+
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/require"
+	"github.com/vinicciussantos/arquitetura-hexagonal/application"
 )	
 
-func testProductEnable(t *testing.T) {
+func TestProductEnable(t *testing.T) {
 	product := application.Product{
+		Name: "Test",
+		Status: application.DISABLED,
 		Price: 10.0,
 	}
 
 	err := product.Enable()
 	require.Nil(t, err)
-	require.Equal(t, application.ENABLED, product.Status)
+	
+	product.Price = 0
+	err = product.Enable()
+	require.Equal(t, "The price must be greater than zero to enable the product", err.Error())
+}
+
+func TestProductDisable(t *testing.T) {
+	product := application.Product{
+		Name: "Test",
+		Status: application.ENABLED,
+		Price: 0,
+	}
+
+	err := product.Disable()
+	require.Nil(t, err)
+	
+	product.Price = 10
+	err = product.Disable()
+	require.Equal(t, "The price must be zero to disable the product", err.Error())
+}
+
+func TestProductIsValid(t *testing.T) {
+	product := application.Product{
+		ID: uuid.NewV4().String(), 
+		Name: "Test",
+		Status: application.DISABLED,
+		Price: 10.0,
+	}
+
+	fmt.Println(product)
+
+	_, err := product.IsValid()
+	require.Nil(t, err)
+	
+	product.Status = "invalid"
+	_, err = product.IsValid()
+	require.Equal(t, "The status must be enabled or disabled", err.Error())
+
+	product.Status = application.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = application.ENABLED
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, "The price must be greater than zero", err.Error())
 }
