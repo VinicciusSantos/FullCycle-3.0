ea2dea9a3052c37710f3ac08408e323b529713ff
Vinícius Guedes <comercial.viniciusguedes@gmail.com>
2023-09-05 10:14:23 -0300

    [cap 13] ✉️ | package de eventos

---
+++ 13 | EDA - Event Driven Architecture ✉️/pkg/events/event_dispatcher.go
@@ -0,0 +1,65 @@
+package events
+
+import "errors"
+
+type EventDispatcher struct {
+	handlers map[string][]EventHandlerInterface
+}
+
+func NewEventDispatcher() *EventDispatcher {
+	return &EventDispatcher{
+		handlers: make(map[string][]EventHandlerInterface),
+	}
+}
+
+func (d *EventDispatcher) Register(eventName string, handler EventHandlerInterface) error {
+	if _, ok := d.handlers[eventName]; ok {
+		for _, h := range d.handlers[eventName] {
+			if h == handler {
+				return errors.New("handler already registered")
+			}
+		}
+	}
+
+	d.handlers[eventName] = append(d.handlers[eventName], handler)
+	return nil
+}
+
+func (d *EventDispatcher) Has(eventName EventInterface, handler EventHandlerInterface) bool {
+	if _, ok := d.handlers[eventName.GetName()]; ok {
+		for _, h := range d.handlers[eventName.GetName()] {
+			if h == handler {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func (d *EventDispatcher) Clear() error {
+	d.handlers = make(map[string][]EventHandlerInterface)
+	return nil
+}
+
+func (d *EventDispatcher) Dispatch(event EventInterface) error {
+	if handlers, ok := d.handlers[event.GetName()]; ok {
+		for _, handler := range handlers {
+			go handler.Handle(event)
+		}
+	}
+
+	return nil
+}
+
+func (d *EventDispatcher) Remove(eventName string, handler EventHandlerInterface) error {
+	if _, ok := d.handlers[eventName]; ok {
+		for i, h := range d.handlers[eventName] {
+			if h == handler {
+				d.handlers[eventName] = append(d.handlers[eventName][:i], d.handlers[eventName][i+1:]...)
+			}
+		}
+	}
+
+	return nil
+}
